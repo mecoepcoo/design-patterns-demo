640eb2dd38a37ae076dbbae7818f8f68e650f10b
Tianzhen <340349237@qq.com>
2019-04-04 23:56:42 +0800

    修改配置

---
+++ eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "useJSXTextNode": true,
+    "project": "./tsconfig.json",
+    "tsconfigRootDir": "./",
+    "extraFileExtensions": []
+  }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const gutil = require('gulp-util'); // 使log的输出格式与gulp统一
+const del = require('del');
+const watch = require('gulp-watch');
 
-gulp.task('default', function () {
+gulp.task('main', () => {
   const tsResult = gulp.src('demos/**/*.ts')
     .pipe(ts({
       noImplicitAny: true,
       target: 'ES5',
       // out: 'output.js'
-    }));
+    }))
+    .on('error', showError);
   return tsResult.js.pipe(gulp.dest('built'));
 });
+
+gulp.task('clean', cb => {
+  return del([
+    'built/*',
+  ], cb);
+});
+
+gulp.task('watch', gulp.series(() => {
+  return watch('demos/**/*.ts', () => {
+    gulp.start('default');
+  });
+}));
+
+gulp.task('default', gulp.series('clean', 'main'));
+
+function showError(error) {
+  gutil.log(gutil.colors.red('ERROR!'), error.message);
+  this.emit('end');
+}
